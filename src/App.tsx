e97ff5408349652042d1e177208e419d0a1863b6
shumik323 <shumik323@gmail.com>
2023-04-25 21:11:43 +0300

    chore: initialized project

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Photo gallery</h1>
+      <div className="card">
+        test
+      </div>
+      <p className="card-footer">
+        test
+      </p>
+    </>
+  )
+}
+
+export default App
