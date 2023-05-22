b90ea14fbf8dc0da49d4b7385d6ecd3a2e3cde2c
shumik323 <shumik323@gmail.com>
2023-05-22 22:41:46 +0300

    feat: add routing, add code splitting(lazy)

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
-import React from 'react';
-import Counter from "./components/Counter";
+import React, {Suspense} from 'react';
+import {Link, Route, Routes} from 'react-router-dom';
 import './index.scss'
+import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
+import {MainPageAsync} from "./pages/MainPage/MainPage.async";
 
 const App = () => {
     return (
         <div className="app">
-            Lorem ipsum dolor sit amet.
-            <Counter />
+            <Link to={'/'}>Home page</Link>
+            <Link to={'/about'}>About page</Link>
+            <Suspense fallback={<div>Loading...</div>}>
+                <Routes>
+                    <Route path={'/about'} element={<AboutPageAsync />}/>
+                    <Route path={'/'} element={<MainPageAsync />}/>
+                </Routes>
+            </Suspense>
         </div>
     );
 };
