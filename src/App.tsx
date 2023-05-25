d12f90371b4ae540abfbeeb9c725b494bed03648
shumik323 <shumik323@gmail.com>
2023-05-25 20:15:37 +0300

    feat: add helpers fn

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,12 @@ import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
 import {MainPageAsync} from "./pages/MainPage/MainPage.async";
 import {Theme, ThemeContext} from "./theme/ThemeContext";
 import {useTheme} from "./theme/useTheme";
+import {classNames} from "./helpers/classNames/classNames";
 
 const App = () => {
 const { theme, toggleTheme } = useTheme();
     return (
-        <div className={`app ${theme}`}>
+        <div className={classNames('app', {}, [theme])}>
             <button onClick={toggleTheme}>TOOGLE</button>
             <Link to={'/'}>Home page</Link>
             <Link to={'/about'}>About page</Link>
