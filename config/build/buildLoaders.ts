842dbf6dff96959b4afffe3affa27b20c2703cf2
shumik323 <shumik323@gmail.com>
2023-05-29 14:59:40 +0300

    feat: add experemantal exact plugin

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -2,49 +2,73 @@ import webpack from "webpack";
 import MiniCssExtractPlugin from "mini-css-extract-plugin";
 import {BuildOptions} from "./types/config";
 
-export function buildLoaders(options: BuildOptions): webpack.RuleSetRule[] {
-    // if ts loader - dont babel
-    const typescriptLoader = {
-            test: /\.tsx?$/,
-            use: 'ts-loader',
-            exclude: /node_modules/,
-    };
+export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
 
     const svgLoader = {
         test: /\.svg$/,
         use: ['@svgr/webpack'],
-    };
+    }
 
-    const fileLoader = {
-        test: /\.(png|jpe?g|gif|woff2|woff)$/i,
-        use: [{loader: 'file-loader'}],
-    };
+    const babelLoader = {
+        test: /\.(js|jsx|tsx)$/,
+        exclude: /node_modules/,
+        use: {
+            loader: "babel-loader",
+            options: {
+                presets: ['@babel/preset-env'],
+                "plugins": [
+                    [
+                        "i18next-extract",
+                        {
+                            locales: ['ru', 'en'],
+                            keyAsDefaultValue: true
+                        }
+                    ],
+                ]
+            }
+        }
+    }
 
-    const styleLoader = {
+    const cssLoader = {
         test: /\.s[ac]ss$/i,
         use: [
-            options.isDev
-                ? 'style-loader'
-                : MiniCssExtractPlugin.loader,
+            isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
             {
-                loader:"css-loader",
+                loader: "css-loader",
                 options: {
                     modules: {
                         auto: (resPath: string) => Boolean(resPath.includes('.module.')),
-                        localIdentName: options.isDev
+                        localIdentName: isDev
                             ? '[path][name]__[local]--[hash:base64:5]'
-                            : '[hash:base64:8]',
-                    }
-                },
+                            : '[hash:base64:8]'
+                    },
+                }
             },
             "sass-loader",
-        ]
-    };
+        ],
+    }
+
+    // Если не используем тайпскрипт - нужен babel-loader
+    const typescriptLoader = {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+    }
+
+    const fileLoader = {
+        test: /\.(png|jpe?g|gif|woff2|woff)$/i,
+        use: [
+            {
+                loader: 'file-loader',
+            },
+        ],
+    }
 
     return [
-        typescriptLoader,
-        styleLoader,
-        svgLoader,
         fileLoader,
+        svgLoader,
+        babelLoader,
+        typescriptLoader,
+        cssLoader,
     ]
 }
