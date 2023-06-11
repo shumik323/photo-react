223763329c1131b4438cb78b457b368fcdd6f36c
shumik323 <shumik323@gmail.com>
2023-06-11 22:33:03 +0300

    feat: add max-len rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -41,6 +41,9 @@ module.exports = {
         'i18next/no-literal-string': ['error', {
             markupOnly: true,
         }],
+        'max-len': ['error', {
+            ignoreComments: true,
+        }],
     },
     globals: {
         __IS_DEV__: true,
