abb279ba86ba61e96e9582cab12c9cbb5cb099eb
i.shumeiko <i.shumeiko@amarkets.com>
2023-09-07 12:56:06 +0300

    fix: fix ci

--- config/build/buildWebpackConfig.ts
+++ config/build/buildWebpackConfig.ts
@@ -1,12 +1,14 @@
 import webpack from 'webpack';
+import path from 'path';
+import { BuildOptions } from './types/config';
 import { buildPlugins } from './buildPlugins';
 import { buildLoaders } from './buildLoaders';
 import { buildResolvers } from './buildResolvers';
-import { BuildOptions } from './types/config';
 import { buildDevServer } from './buildDevServer';
 
 export function buildWebpackConfig(options: BuildOptions): webpack.Configuration {
-    const { mode, paths, isDev } = options;
+    const { paths, mode, isDev } = options;
+
     return {
         mode,
         entry: paths.entry,
